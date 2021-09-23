04adb4246b5711109b52ba100a45bdcfce42cb64
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-23 23:41:56 +0530

    Quotient and remainder is added

---
+++ QandR.sh
@@ -0,0 +1,24 @@
+using System;
+
+namespace QuotientandRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int dividend = 20;
+            int divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend: {0} Divisor: {1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+
+}
